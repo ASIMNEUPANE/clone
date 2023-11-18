fcaedff6f16585670b038c37097c47112141e7d8
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-18 23:37:32 +0545

    getbyId

--- server/modules/users/controller.js
+++ server/modules/users/controller.js
@@ -1,52 +1,53 @@
-const model = require ('./model')
+const model = require("./model");
 const bcrypt = require("bcrypt");
 
-const create = async(payload)=>{
-    const {password,roles,...rest}= payload
-    rest.password = await bcrypt.hash(password, process.env.SALT_ROUND)
-    rest.isActive = true;
-    rest.isEmailVerified= true;
-    await model.create(rest)
-    
-}
+const create = async (payload) => {
+  const { password, roles, ...rest } = payload;
+  rest.password = await bcrypt.hash(password, process.env.SALT_ROUND);
+  rest.isActive = true;
+  rest.isEmailVerified = true;
+  await model.create(rest);
+};
 
-const list = async({page,limit})=>{
-    page = Number(page) || 1;
-    limit = Number(limit) || 1
-   return await model.aggregate(
-        [
+const list = async ({ page, limit }) => {
+  page = Number(page) || 1;
+  limit = Number(limit) || 1;
+  return await model
+    .aggregate([
+      {
+        $facet: {
+          metadata: [
             {
-              '$facet': {
-                'metadata': [
-                  {
-                    '$count': 'total'
-                  }
-                ], 
-                'data': [
-                  {
-                    '$skip': (page - 1) * limit
-                  }, {
-                    '$limit': limit
-                  }
-                ]
-              }
-            }, {
-              '$addFields': {
-                'total': {
-                  '$arrayElemAt': [
-                    '$metadata.total', 0
-                  ]
-                }
-              }
-            }, {
-              '$project': {
-                'metadata': 0
-              }
-            }
-          ]
-    ).allowDiskUse(true)
-    
-  
-}
+              $count: "total",
+            },
+          ],
+          data: [
+            {
+              $skip: (page - 1) * limit,
+            },
+            {
+              $limit: limit,
+            },
+          ],
+        },
+      },
+      {
+        $addFields: {
+          total: {
+            $arrayElemAt: ["$metadata.total", 0],
+          },
+        },
+      },
+      {
+        $project: {
+          metadata: 0,
+        },
+      },
+    ])
+    .allowDiskUse(true);
+};
+const getById = async (id) => {
+  await model.findOne({ _id: id });
+};
 
-module.exports= {create,list}
+module.exports = { create, list, getById };
--- server/modules/users/routes.js
+++ server/modules/users/routes.js
@@ -6,6 +6,11 @@ router.get('/', async(req,res,next)=>{
 const result = await controller.list(page,limit)
 res.json({data:result, msg:"success"})
 })
+router.get('/:id', async(req,res,next)=>{
+
+const result = await controller.getById(req.params.id)
+res.json({data:result, msg:"success"})
+})
 
 
 module.exports= router
