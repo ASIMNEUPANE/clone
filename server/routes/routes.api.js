f928393f9ea4a548685fcb9712bc33398eff5c32
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-19 22:01:20 +0545

    added category to the routes

--- server/routes/routes.api.js
+++ server/routes/routes.api.js
@@ -1,9 +1,11 @@
 const router  = require('express').Router()
 const authRouter = require('../modules/auth/routes')
 const userRouter = require('../modules/users/routes')
+const categoryRouter = require('../modules/categories/routes')
 
 router.use('/auth', authRouter)
 router.use('/users', userRouter)
+router.use('/categories', categoryRouter)
 
 router.all('*', (req,res,next)=>{
     res.json({data:"", msg:"Route not found"})
