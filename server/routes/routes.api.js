57adda2db5e0b3dee7cf4480087ff11ba6e9fb66
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-26 21:21:25 +0545

    Add order router

--- server/routes/routes.api.js
+++ server/routes/routes.api.js
@@ -3,11 +3,13 @@ const authRouter = require('../modules/auth/routes')
 const userRouter = require('../modules/users/routes')
 const categoryRouter = require('../modules/categories/routes')
 const productRouter = require('../modules/products/routes')
+const orderRouter = require('../modules/orders/routes')
 
 router.use('/auth', authRouter)
 router.use('/users', userRouter)
 router.use('/categories', categoryRouter)
 router.use('/products', productRouter)
+router.use('/orders', orderRouter)
 
 router.all('*', (req,res,next)=>{
     res.json({data:"", msg:"Route not found"})
