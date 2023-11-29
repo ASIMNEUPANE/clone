c5d37d117c231b04a80930d12809913cce4cad63
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-29 21:58:20 +0545

    Add Redux store provider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import Layout from "./Layouts/Layout.jsx";
 import Home from "../src/page/Home.jsx";
 import About from "../src/page/About.jsx";
@@ -18,13 +22,14 @@ const router = createBrowserRouter([
       { path: "about", element: <About /> },
       { path: "cart", element: <Cart /> },
       { path: "products", element: <Products /> },
-    
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
