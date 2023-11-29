9fc4eb1f46c7cc113c64e2fce65d5f7561966a3f
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-29 13:07:11 +0545

    Update routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Navbar } from "./layouts/navbar";
+
+// Layout
+import Layout from "./layouts/layout";
+
 import { Home } from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navbar />} />
-          <Route index element={<Home />} />
+          <Route path="/" element={<Layout />} />
+          <Route index={true} element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </BrowserRouter>
     </div>
