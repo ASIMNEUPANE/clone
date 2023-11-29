82b0a05e6987c00448cf2045c6bebf96ad5154e2
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-11-29 21:57:57 +0545

    Add constants and API utility file

---
+++ client/src/constants/index.jsx
@@ -0,0 +1,10 @@
+export const SERVER_URL = "http://localhost:4444";
+
+const version = "/api/v1";
+export const URLS = {
+  AUTH: version + "/auth",
+  USERS: version + "/users",
+  ORDERS: version + "/orders",
+  CATEGORIES: version + "/categories",
+  PRODUCTS: version + "/products",
+};
---
+++ client/src/utils/API.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+import { SERVER_URL } from "../constants";
+
+const API = axios.create({
+  baseURL: SERVER_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: localStorage.getItem("user")
+      ? "Bearer " + JSON.parse(localStorage.getItem("user")).token
+      : null,
+  },
+//   headers: { 
+//     "Content-Type": "application/json",
+//     Authorization: localStorage.getItem("access_token")
+//       ? "Bearer " + localStorage.getItem("access_token")
+//       : null,
+//  },
+});
+
+export default API;
