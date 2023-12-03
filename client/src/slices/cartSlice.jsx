cdb438f82dbe4d45ab60b77b8e6985d13182a8ff
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-03 22:09:46 +0545

    Add cartSlice.jsx with addToCart functionality

---
+++ client/src/slices/cartSlice.jsx
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+  quantity: 0,
+};
+console.log(initialState,"HEllo")
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      console.log(action);
+      const itemsInCart = state.cart.find((item) => {
+        item._id === action.payload._id;
+      });
+      if (itemsInCart) {
+        if (itemsInCart.quantity < action.payload.quantity) {
+          itemsInCart.quantity++;
+          state.quantity++;
+        }} else {
+          state.cart.push({ ...action.payload, quantity: 1 });
+          state.quantity++;
+        
+      }
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+export const carReducer = cartSlice.reducer;
