bce2e4e8f1916e332f567309d70c5afdaa8e8982
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-04 12:37:20 +0545

    Add Redux Persist integration to store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,9 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { store } from "../store.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, newStore } from "../store.jsx";
+
 import Layout from "./Layouts/Layout.jsx";
 import Home from "../src/page/Home.jsx";
 import About from "../src/page/About.jsx";
@@ -28,7 +30,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate loading={null} persistor={newStore}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
