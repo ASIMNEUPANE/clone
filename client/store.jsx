a98475317253a97192f2ea0bbe05828d95248de8
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-04 12:37:13 +0545

    Refactor Redux store configuration and add persistence

--- client/store.jsx
+++ client/store.jsx
@@ -1,12 +1,38 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  PAUSE,
+  PERSIST,
+  REGISTER,
+  PURGE,
+  FLUSH,
+  REHYDRATE,
+} from "redux-persist";
+import { autoMergeLevel2 } from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import storage from "redux-persist/lib/storage";
 
+import { productReducer } from "./src/slices/productsSlice";
+import { cartReducer } from "./src/slices/cartSlice";
 
-import { productReducer } from "./src/slices/productsSlice"
-import { carReducer } from "./src/slices/cartSlice"
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const cartPersistedReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
-    reducer:{
-        products:productReducer,
-        cart:carReducer
-    }
-})
+  reducer: {
+    products: productReducer,
+    cart: cartPersistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [PAUSE, PERSIST, REGISTER, PURGE, FLUSH, REHYDRATE],
+      },
+    }),
+});
+export const newStore = persistStore(store);
