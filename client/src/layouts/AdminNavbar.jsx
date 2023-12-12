d84565f4d899b697b6e1fe6fb0f3bffc6dabeaf1
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-13 03:02:27 +0545

    Refactor AdminNavbar component to include logout functionality

--- client/src/layouts/AdminNavbar.jsx
+++ client/src/layouts/AdminNavbar.jsx
@@ -1,26 +1,38 @@
-import { Link, NavLink } from "react-router-dom";
-
+import { Link,  useNavigate } from "react-router-dom";
+import { CiLogout } from "react-icons/ci";
+import { useDispatch, useSelector } from "react-redux";
+import { setLogOut } from "../slices/authSlice";
+import { removeToken } from "../utils/session";
 const AdminNavbar = () => {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  
+  const handleLockOut = () => {
+    dispatch(setLogOut());
+    removeToken();
+    navigate("/login");
+  };
   return (
     <nav className=" bg-orange-500 text-white p-4 rounded-sm gap-2">
       <div className="container mx-auto flex gap-40 items-center">
-        <Link to="/" className="text-lg font-semibold">
-          Admin
-        </Link>
-        <NavLink
-          to="/products"
-          className={({ isActive }) => {
-            ` ${isActive ? "text-orange-700" : "text-gray-700"}`;
-          }}
-        >
+        <Link to="/admin/dashboard" className="text-lg font-semibold">
+          Admin {user?.name}
+        </Link>       
+        <Link to="/admin/products" className="text-lg">
           Products
-        </NavLink>
-        <Link to="/about" className="text-lg">
-          About
         </Link>
-        <Link to="/cart" className="text-lg">
-          Cart
+        <Link to="/admin/dashboard" className="text-lg">
+          Users
         </Link>
+        <Link to="/admin/dashboard" className="text-lg">
+          Categories
+        </Link>
+        <button onClick={handleLockOut}>
+          <Link to="/admin/dashboard" className="text-lg">
+            <CiLogout />
+          </Link>
+        </button>
       </div>
     </nav>
   );
