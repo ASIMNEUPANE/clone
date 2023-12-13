0806c0f00efca3aa65417ea71eb33ee8c700e4d6
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-14 03:01:37 +0545

    Refactor useCategory hook

--- client/src/hooks/useCategory.jsx
+++ client/src/hooks/useCategory.jsx
@@ -3,23 +3,60 @@ import API from "../utils/API";
 import { URLS } from "../constants";
 
 const useCategory = () => {
-  const [ error, setError ] = useState("");
-  const [ loading, setLoading ] = useState(false);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const list = async() => {
+  const list = async () => {
     try {
       setLoading(true);
-      const {data} = await API.get(URLS.CATEGORIES)
-    
-      return data.data
+      const { data } = await API.get(URLS.CATEGORIES);
+
+      return data.data;
     } catch (e) {
-        setError(e)
-    }finally{
-        setLoading(false)
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const getById = async (id) => {
+    try {
+      setLoading(true);
+      const { data } = await API.get(URLS.CATEGORIES+'/'+id);
+
+      return data.data;
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const updateById = async (id,payload) => {
+    try {
+      setLoading(true);
+      const result = await API.get(URLS.CATEGORIES+'/'+id,payload);
+
+      return result;
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const deleteById = async (id) => {
+    try {
+      setLoading(true);
+      const result = await API.delete(`${URLS.CATEGORIES}/${id}`);
+     
+      return result;
+      
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
     }
   };
 
-  return {list}
+  return { list,getById,deleteById,updateById };
 };
 
 export default useCategory;
