08e0dd3aa47ed7c8a3b5925a75a38c5c6fabfee1
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-15 22:59:58 +0545

    Update category controller to sort by name

--- server/modules/categories/controller.js
+++ server/modules/categories/controller.js
@@ -14,14 +14,18 @@ const create = async (payload) => {
 const list = async (limit, page, search) => {
 
   page = parseInt(page) || 1;
-  limit = parseInt(limit) || 3;
-  console.log(page,limit)
+  limit = parseInt(limit) || 5;
   const { isArchived } = search;
   const response =  await model
     .aggregate([
       {
         $match: { isArchived: Boolean(isArchived) || false },
       },
+      {
+        $sort: {
+          name: -1,
+        },
+      }, 
       {
         $facet: {
           metadata: [
@@ -61,11 +65,14 @@ const list = async (limit, page, search) => {
 };
 
 const getById=async(id)=>{
-  await model.findOne({_id:id})
+ return await model.findOne({_id:id})
 }
 const updateById=async(id,payload)=>{
-  payload.slug = slugGenerator(payload.name)
-  await model.findOneAndUpdate({_id:id}, payload, {new:true})
+  if(payload.name){
+    payload.slug = await slugGenerator(payload.name)
+  }
+  
+ return await model.findOneAndUpdate({_id:id}, payload, {new:true})
 }
 
 const deleteById= async(id)=>{
