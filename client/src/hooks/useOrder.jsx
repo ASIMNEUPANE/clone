d0a26a42767eef4a376bb9c2ac6133d0aabda821
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-18 02:20:23 +0545

    Add CRUD operations and approve function to useOrder hook

--- client/src/hooks/useOrder.jsx
+++ client/src/hooks/useOrder.jsx
@@ -17,10 +17,42 @@ const useOrder = () => {
       setLoading(false);
     }
   };
+  const getById = async (id) => {
+    try {
+      setLoading(true);
+      const { data } = await API.get(`${URLS.ORDERS}/${id}`);
+      return data.data;
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const updateById = async (id, payload) => {
+    try {
+      setLoading(true);
+      const resp = await API.put(`${URLS.ORDERS}/${id}`, payload);
+      return resp;
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const create = async (payload) => {
+    try {
+      setLoading(true);
+      const resp = await API.post(URLS.ORDERS, payload);
+      return resp.data;
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
   const deleteById = async (id) => {
     try {
       setLoading(true);
-      console.log(id)
       const resp = await API.delete(`${URLS.ORDERS}/${id}`);
       return resp.data;
     } catch (e) {
@@ -29,8 +61,19 @@ const useOrder = () => {
       setLoading(false);
     }
   };
+  const approve = async (id, payload) => {
+    try {
+      setLoading(true);
+      const resp = await API.patch(`${URLS.ORDERS}/status/${id}`, payload);
+      return resp;
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  return { list ,deleteById};
+  return { list, getById, deleteById, create, updateById, approve };
 };
 
 export default useOrder;
