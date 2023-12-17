f1d37045be0ead38d5d33d7648862377b1f57db6
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-18 00:04:10 +0545

    Add product selection and quantity input in order form

--- client/src/page/admin/orders/add.jsx
+++ client/src/page/admin/orders/add.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { useState } from "react";
+import { fetchProducts } from "../../../slices/productsSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function add() {
+  const dispatch = useDispatch();
+  const { products: allProducts } = useSelector((state) => state.products);
   const [order, setOrder] = useState({
     name: "",
     email: "",
@@ -10,12 +14,27 @@ export default function add() {
     amount: "",
   });
 
+  const [item, setItem] = useState({
+    product: "",
+    quantity: 0,
+    amount: 0,
+    price: 0,
+  });
+
+  const fetInt = useCallback(async () => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetInt();
+  }, [fetInt]);
+
   return (
-    <div class="grid grid-cols-4 gap-3 ">
-      <div class="bg-gray-200">
+    <div class="grid grid-cols-4 gap-3 m-3 ">
+      <div class="bg-gray-200 rounded-sm">
         {/* <form onSubmit={handleSubmit}> */}
         <form>
-          <div class="mb-4">
+          <div class="mb-4 ">
             <label
               for="categoryName"
               class="block text-sm font-medium text-gray-600"
@@ -135,8 +154,82 @@ export default function add() {
         </form>
       </div>
 
-      <div class="bg-red-500 col-span-1">h</div>
-      <div class="bg-red-500 col-span-2">h</div>
+      <div class="bg-gray-200 col-span-1 rounded-sm">
+        <form>
+          <div class="mb-4">
+            <label
+              for="categoryName"
+              class="block text-sm font-medium text-gray-600"
+            >
+              Products
+            </label>
+            <select
+              onChange={(e) => {
+                setItem((prev) => {
+                  return {
+                    ...prev,
+                    product: e.target.value,
+                    price: allProducts.find(
+                      (item) => item?._id === e.target.value
+                    ).price,
+                  };
+                });
+              }}
+            >
+              <option>Open this select memu</option>
+              {allProducts && allProducts?.length > 0
+                ? allProducts.map((prod) => {
+                    return (
+                      <option key={prod?._id} value={prod?._id}>
+                        {prod?.name}
+                      </option>
+                    );
+                  })
+                : null}
+            </select>
+          </div>
+          <div class="mb-4">
+            <label
+              for="categoryName"
+              class="block text-sm font-medium text-gray-600"
+            >
+              Qunatity
+            </label>
+            <input
+              type="text"
+              value={item.quantity}
+              onChange={(e) => {
+                setItem((prev) => {
+                  return {
+                    ...prev,
+                    quantity: e.target.value,
+                    amount: Number(e.target.value) * Number(item?.price),
+                  };
+                });
+              }}
+            />
+          </div>
+          <div class="mb-4">
+            <label
+              for="categoryName"
+              class="block text-sm font-medium text-gray-600"
+            >
+              price
+            </label>
+            <input type="text" value={item?.price} />
+          </div>
+          <div class="mb-4">
+            <label
+              for="categoryName"
+              class="block text-sm font-medium text-gray-600"
+            >
+              amount
+            </label>
+            <input type="text" value={item?.amount} />
+          </div>
+        </form>
+      </div>
+      <div class="bg-red-500 col-span-2 rounded-sm">h</div>
     </div>
   );
 }
