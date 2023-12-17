5b3f69ef13b85da2c38fd6f12f9b15faf2571da8
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-18 02:19:55 +0545

    Add functionality to add and remove products in the order form

--- client/src/page/admin/orders/add.jsx
+++ client/src/page/admin/orders/add.jsx
@@ -1,10 +1,14 @@
 import React, { useCallback, useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { fetchProducts } from "../../../slices/productsSlice";
+import useOrder from "../../../hooks/useOrder";
 import { useSelector, useDispatch } from "react-redux";
 
 export default function add() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { create } = useOrder();
   const { products: allProducts } = useSelector((state) => state.products);
   const [order, setOrder] = useState({
     name: "",
@@ -20,6 +24,52 @@ export default function add() {
     amount: 0,
     price: 0,
   });
+  const [products, setProducts] = useState([]);
+
+  const handleAddingProducts = (e) => {
+    e.preventDefault();
+    if (!item || !item.product || !item?.quantity) return;
+    const newProduct = [...products, item];
+    setProducts(newProduct);
+    setOrder((prev) => {
+      return {
+        ...prev,
+        amount: newProduct.reduce((acc, item) => acc + item?.amount, 0),
+      };
+    });
+    setItem({
+      product: "",
+      quantity: 0,
+      amount: 0,
+      price: 0,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const payload = order;
+      payload.products = products;
+      const result = await create(payload);
+      if (result.msg === "success") {
+        alert("Order created successfully");
+        navigate("/admin/orders");
+      }
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  const removeProduct = (id) => {
+    const newProduct = products.filter((product) => product.product !== id);
+    setProducts(newProduct);
+    setOrder((prev) => {
+      return {
+        ...prev,
+        amount: newProduct.reduce((acc, item) => acc + item?.amount, 0),
+      };
+    });
+  };
 
   const fetInt = useCallback(async () => {
     dispatch(fetchProducts());
@@ -32,8 +82,7 @@ export default function add() {
   return (
     <div class="grid grid-cols-4 gap-3 m-3 ">
       <div class="bg-gray-200 rounded-sm">
-        {/* <form onSubmit={handleSubmit}> */}
-        <form>
+        <form onSubmit={handleSubmit}>
           <div class="mb-4 ">
             <label
               for="categoryName"
@@ -112,13 +161,15 @@ export default function add() {
               PaymentMethod
             </label>
             <select
-              value={order.paymentMethod}
+              value={order?.paymentMethod}
               onChange={(e) => {
                 setOrder((prev) => {
                   return { ...prev, paymentMethod: e.target.value };
                 });
               }}
             >
+              <option>Open this select menu</option>
+
               <option value="COD">COD</option>
               <option value="STRIPE">STRIPE</option>
             </select>
@@ -136,17 +187,14 @@ export default function add() {
               name="categoryName"
               disabled
               class="mt-1 p-2 w-full border rounded-md"
-              // value={category}
-              // onChange={(e) => {
-              //   setCategory(e.target.value);
-              // }}
+              value={order?.amount}
             />
           </div>
 
           <div class="flex items-center justify-end">
             <button
               type="submit"
-              class="bg-blue-500 text-white px-4 py-2 rounded-md"
+              class="bg-gray-200 text-white px-4 py-2 rounded-md"
             >
               Create Category
             </button>
@@ -155,7 +203,7 @@ export default function add() {
       </div>
 
       <div class="bg-gray-200 col-span-1 rounded-sm">
-        <form>
+        <form onSubmit={handleAddingProducts}>
           <div class="mb-4">
             <label
               for="categoryName"
@@ -227,9 +275,43 @@ export default function add() {
             </label>
             <input type="text" value={item?.amount} />
           </div>
+          <button type="submit">Add product</button>
         </form>
       </div>
-      <div class="bg-red-500 col-span-2 rounded-sm">h</div>
+      <div class="bg-gray-200 col-span-2 rounded-sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Amount</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products &&
+            products.map((product, idx) => {
+              return (
+                <tr key={idx}>
+                  <td>{idx + 1}</td>
+                  <td>
+                    {
+                      allProducts.find((item) => item?._id === product?.product)
+                        .name
+                    }
+                  </td>
+                  <td>{product?.quantity}</td>
+                  <td>{product?.price}</td>
+                  <td>{product?.amount}</td>
+                  <td onClick={() => removeProduct(product?.product)}>
+                    delete
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </div>
     </div>
   );
 }
