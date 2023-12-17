80e4922e17f5973a12d6254f294fc890180d10eb
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-17 23:24:30 +0545

    Update navbar styling

--- client/src/layouts/ENavbar.jsx
+++ client/src/layouts/ENavbar.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import { CiLogin } from "react-icons/ci";
 const ENavbar = () => {
   return (
-    <nav className=" bg-orange-500 text-white p-4 rounded-sm gap-2">
+    <nav className="bg-gradient-to-r from-blue-500 to-purple-500 text-white p-4 shadow-lg  rounded-sm gap-2">
       <div className="container mx-auto flex gap-40 items-center">
         <Link to="/" className="text-lg font-semibold">
           Home
