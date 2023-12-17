91d12ade6a16d4f0e72ec1f73681c22f17a8bb2d
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-17 23:24:37 +0545

    Update footer styling and content

--- client/src/layouts/Footer.jsx
+++ client/src/layouts/Footer.jsx
@@ -2,25 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto text-center">
-        <p className="text-sm">© 2023 Your Company. All rights reserved.</p>
-        <div className="mt-2">
-          <a
-            href="#"
-            className="text-gray-400 hover:text-white mx-2 transition duration-300"
-          >
-            Privacy Policy
-          </a>
-          <span className="text-gray-400 mx-2">|</span>
-          <a
-            href="#"
-            className="text-gray-400 hover:text-white mx-2 transition duration-300"
-          >
-            Terms of Service
-          </a>
-        </div>
-      </div>
+    <footer className="fixed bottom-0 left-0 w-full text-center  bg-gradient-to-r from-blue-500 to-purple-500 text-white p-4 shadow-lg">
+      <p>&copy; 2023 Your Company</p>
     </footer>
   );
 }
